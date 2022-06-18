3f2f9310ac7c93e5908d0793342ae3fc7fd9e3b3
Douglas J Bothwell <doug.bothwell.opnet@gmail.com>
2022-06-18 07:41:28 -0700

    hotfix ABC-234

--- frontEndApp.py
+++ frontEndApp.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
+feapp = Flask(__name__)
 
-@app.route('/')
+@feapp.route('/')
 def hello_world():
     return 'sgeTestOne (my-feature-branch) says Hello, Docker!'
